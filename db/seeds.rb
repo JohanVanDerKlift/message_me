d2d313d73defd7a5b30a69d283047289610416b1
Klippie <info@johanvanderklift.nl>
2020-09-09 16:19:33 +0200

    Create message partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Johan", password: "Feyenoord1")
+User.create(username: "Ilse", password: "password")
+User.create(username: "Yara", password: "password")
+User.create(username: "Jan", password: "password")
+User.create(username: "Piet", password: "password")
